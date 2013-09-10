fcb3576de1bb01a8d56527b2526aeb75b20d46ad
Andrew Jiang <andrew.h.jiang@gmail.com>
2013-09-09 21:32:43 -0400

    Added forms

--- fbclone_app/forms.py
+++ fbclone_app/forms.py
@@ -12,4 +12,42 @@ class UserCreateForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
     password2 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
 
+    def is_valid(self):
+    	form = super(UserCreateForm, self).is_valid()
+    	for f, error in self.errors.iteritems():
+    		if f != '__all__':
+    			self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+    	return form
 
+    class Meta:
+    	fields = ['email', 'username', 'first_name', 'last_name', 'password1', 'password2']
+    	model = User
+
+class AuthenticateForm(AuthenticationForm):
+	username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+	password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+
+	def is_valid(self):
+		form = super(AuthenticateForm, self).is_valid()
+		for f, error in self.errors.iteritems():
+			if f != '__all__':
+				self.fields[f].widget.attrs.update({'class': 'error fbcloneText'})
+		return form
+
+	class Meta:
+		model = Share
+		exclude = ('user',)
+
+class ShareForm(forms.ModelForm):
+	content = forms.CharField(required=True, widget=forms.widget.Textarea(attrs={'class': 'shareText'}))
+
+	def is_valid(self):
+		form = super(ShareForm, self).is_valid()
+		for f in self.errors.iterkeys():
+			if f != '__all__':
+				self.fields[f].widget.attrs.update({'class': 'error shareText'})
+			return form
+
+		class Meta:
+			model = Share
+			exclude = ('user',)
